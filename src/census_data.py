911b0f700ff16ae0f200bfd38cdb1118278ffd0c
Gabriela Rodriguez <103657707+gabis96@users.noreply.github.com>
2022-12-29 15:36:12 +0000

    Update census data class.

--- src/census_data.py
+++ src/census_data.py
@@ -1,4 +1,3 @@
-import src.map_data as md
 import pandas as pd
 
 # Working with Paris Census Data
@@ -8,43 +7,34 @@ class ParisCensusData:
         self.familleDF = pd.read_csv(famille_path, encoding = "utf-8")
         self.populationDF = pd.read_csv(population_path, encoding = "utf-8")
         self.revenusDF = pd.read_csv(revenus_path, encoding = "utf-8")
-        self.mapDF = pd.DataFrame()
         
         self.bad_iris = [751124577, 751124677, 751166177, 751166277, 751166377]
 
-    def map(self, shapefile):
-        indexName = 'DCOMIRIS'
-
-        parisdata = md.mapData(shapefile)
-
-        self.mapDF = parisdata.createGeometryMap(indexName, self.bad_iris)
-        self.mapDF.index = self.mapDF.index.astype(int)    
-
     def build(self):
-        self.select_paris()
-        self.set_index()
-        self.delete_bad_iris()
-        self.filter_data()
+        self._select_paris()
+        self._set_index()
+        self._delete_bad_iris()
+        self._filter_data()
 
-    def select_paris(self):
+    def _select_paris(self):
         # Choose only Paris Data
         self.familleDF = self.familleDF[self.familleDF['IRIS'].astype(str).str.startswith('75', na=False)]
         self.populationDF = self.populationDF[self.populationDF['IRIS'].astype(str).str.startswith('75', na=False)]
         self.revenusDF = self.revenusDF[self.revenusDF['IRIS'].astype(str).str.startswith('75', na=False)]
     
-    def set_index(self):
+    def _set_index(self):
         # Set IRIS as index
         self.familleDF = self.familleDF.set_index('IRIS')
         self.populationDF = self.populationDF.set_index('IRIS')
         self.revenusDF = self.revenusDF.set_index('IRIS')
         
-    def delete_bad_iris(self):
+    def _delete_bad_iris(self):
         # Delete Paris Wood
         self.familleDF = self.familleDF.drop(self.bad_iris)
-        self.populationDF = self.populationDF.drop(self.bad_iris, axis = 0)
+        self.populationDF = self.populationDF.drop(self.bad_iris)
         #self.revenusDF = self.revenusDF.drop(bad_iris, axis = 0)
 
-    def filter_data(self):    
+    def _filter_data(self):    
         # Dejar a todos con la misma cantiadad de Iris
         # self.familleDF = self.familleDF[self.familleDF.index.isin(self.revenusDF.index)]
         # self.populationDF = self.populationDF[self.populationDF.index.isin(self.revenusDF.index)]
